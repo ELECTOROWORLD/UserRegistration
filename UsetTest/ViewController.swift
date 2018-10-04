b44e8ffbf88b767981515cf892d08b184524b1a7
AokiRikuRiku <rikugithub@yahoo.co.jp>
2018-10-04 14:57:25 +0900

    Initial Commit

---
+++ UsetTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UsetTest
+//
+//  Created by 青木　陸 on 2018/10/04.
+//  Copyright © 2018 Aoki Riku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
